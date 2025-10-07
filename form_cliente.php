f6c20ef58dfb25af8b4d3e9554b93deb00649568
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-10-07 08:39:51 -0300

    Exemplo mínimo com Bootstrap

--- form_cliente.php
+++ form_cliente.php
@@ -41,9 +41,10 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadrastro Cliente</title>
+    <link rel="stylesheet" href="bootstrap.min.css">
 </head>
 <body>
-    <h1>Cadrastro cliente</h1>
+    <h1>Cadastro cliente</h1>
 
     <form action="salvar_cliente.php?id=<?php echo $id; ?>" method="POST">
         Nome: <br>
@@ -55,8 +56,9 @@ else {
         Data de nascimento: <br>
         <input type="text" name="nascimento" value="<?php echo $nascimento; ?>"> <br> <br>
         <br>
-        <input type="submit" value="Salvar Cliente">
+        <input type="submit" class="btn btn-primary" value="Salvar Cliente">
 
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
