da8a85e49c06be6594bbb538f653f9efa04ffa6c
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-10-07 08:44:20 -0300

    Create home.php
    Alterada a página de menus do sistema.

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Página Principal</h1>
+
+    <a href="form_produto.php">Cadastrar produto</a>
+    <br>
+    <a href="form_cliente.php"> Cadastrar cliente</a>
+    <br> 
+    <hr>
+    <a href="lista_clientes.php">Lista de clientes</a> <br>
+    <a href="lista_produtos.php">Lista de produtos</a> <br>
+    <a href="lista_produtos_2.php">Lista de produtos (card)</a> <br>
+
+    <br><br><br>
+
+    <a href="deslogar.php">Sair do sistema</a>
+</body>
+</html>
