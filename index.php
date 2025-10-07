933c1cd31e323dda91271538eb2946f635c3ada5
Lucas Faria <lucas.faria@ifgoiano.edu.br>
2025-10-07 08:45:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Página Principal</h1>
+    <h2>Acesso ao sistema</h2>
+    <form action="verificar_login.php" method="POST">
+        E-mail: <br>
+        <input type="text" name="email"> <br><br>
 
-    <a href="form_produto.php">Cadastrar produto</a>
-    <br>
-    <a href="form_cliente.php"> Cadastrar cliente</a>
-    <br> 
-    <hr>
-    <a href="lista_clientes.php">Lista de clientes</a> <br>
-    <a href="lista_produtos.php">Lista de produtos</a> <br>
-    <a href="lista_produtos_2.php">Lista de produtos (card)</a> <br>
+        Senha: <br>
+        <input type="text" name="senha"> <br><br>
+
+        <input type="submit" value="Acessar">
+    </form>
 </body>
 </html>
